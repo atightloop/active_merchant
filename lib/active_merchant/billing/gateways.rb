706643bc2a0f613a3e8df36cc38a3dbe88cdfa33
Kurt kopala <Kurt@atightloop.com>
2016-08-18 14:07:25 -0500

    Update gateways.rb

--- lib/active_merchant/billing/gateways.rb
+++ lib/active_merchant/billing/gateways.rb
@@ -7,7 +7,6 @@ module ActiveMerchant
       gateway_name      = File.basename(filename, '.rb')
       gateway_classname = "#{gateway_name}_gateway".camelize
       gateway_filename  = Pathname.new(filename).relative_path_from(load_path).sub_ext('')
-      logger.debug "filename3 #{filename}"
       autoload(gateway_classname, gateway_filename)
     end
   end
